33894da038ec030a925f2eeec082d58de447d103
giovaness30 <35101491+giovaness30@users.noreply.github.com>
2020-11-16 09:54:01 -0300

    Version 1.4.7
    Alteração id divs widget footer

--- footer-widget.php
+++ footer-widget.php
@@ -5,13 +5,13 @@ if ( is_active_sidebar( 'footer1_sidebar' ) || is_active_sidebar( 'footer2_sideb
             <div class="container">
                 <div class="row">
                     <?php if ( is_active_sidebar( 'footer1_sidebar' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer1_sidebar' ); ?></div>
+                        <div id="footer1" class="col-12 col-md-4"><?php dynamic_sidebar( 'footer1_sidebar' ); ?></div>
                     <?php endif; ?>
                     <?php if ( is_active_sidebar( 'footer2_sidebar' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer2_sidebar' ); ?></div>
+                        <div id="footer2" class="col-12 col-md-4"><?php dynamic_sidebar( 'footer2_sidebar' ); ?></div>
                     <?php endif; ?>
                     <?php if ( is_active_sidebar( 'footer3_sidebar' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer3_sidebar' ); ?></div>
+                        <div id="footer3" class="col-12 col-md-4"><?php dynamic_sidebar( 'footer3_sidebar' ); ?></div>
                     <?php endif; ?>
                 </div>
             </div>
