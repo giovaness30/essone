bb4a91eb95412eca9581ddeaf8a96778407a352e
giovaness30 <35101491+giovaness30@users.noreply.github.com>
2020-11-16 14:26:42 -0300

    version 1.4.8
    Correção da versão 1.4.8;
    Footer Widget

--- footer-widget.php
+++ footer-widget.php
@@ -3,18 +3,16 @@
 if ( is_active_sidebar( 'footer1_sidebar' ) || is_active_sidebar( 'footer2_sidebar' ) || is_active_sidebar( 'footer3_sidebar' ) ) {?>
         <div id="footer-widget" class="py-4 border-top">
             <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <?php if ( is_active_sidebar( 'footer1_sidebar' )) : ?>
-                            <div id="footer1" class="col-12 col-md-4"><?php dynamic_sidebar( 'footer1_sidebar' ); ?></div>
-                        <?php endif; ?>
-                        <?php if ( is_active_sidebar( 'footer2_sidebar' )) : ?>
-                            <div id="footer2" class="col-12 col-md-4"><?php dynamic_sidebar( 'footer2_sidebar' ); ?></div>
-                        <?php endif; ?>
-                        <?php if ( is_active_sidebar( 'footer3_sidebar' )) : ?>
-                            <div id="footer3" class="col-12 col-md-4"><?php dynamic_sidebar( 'footer3_sidebar' ); ?></div>
-                        <?php endif; ?>
-                    </div>
+                <div class="row col-12">
+                    <?php if ( is_active_sidebar( 'footer1_sidebar' )) : ?>
+                        <div id="footer1" class="col-12 col-md-4 col-lg-4"><?php dynamic_sidebar( 'footer1_sidebar' ); ?></div>
+                    <?php endif; ?>
+                    <?php if ( is_active_sidebar( 'footer2_sidebar' )) : ?>
+                        <div id="footer2" class="col-12 col-md-4 col-lg-4"><?php dynamic_sidebar( 'footer2_sidebar' ); ?></div>
+                    <?php endif; ?>
+                    <?php if ( is_active_sidebar( 'footer3_sidebar' )) : ?>
+                        <div id="footer3" class="col-12 col-md-4 col-lg-4"><?php dynamic_sidebar( 'footer3_sidebar' ); ?></div>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
