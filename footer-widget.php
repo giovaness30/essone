ea6ba48fcf7f940fe9cea8c41096eb3aa1df54ca
Giovane Sedano <35101491+giovaness30@users.noreply.github.com>
2021-01-26 22:27:50 -0300

    Version 1.6.0
    -- Correção Posicionamento das áreas de Widget

--- footer-widget.php
+++ footer-widget.php
@@ -4,15 +4,13 @@ if ( is_active_sidebar( 'footer1_sidebar' ) || is_active_sidebar( 'footer2_sideb
         <div id="footer-widget" class="footer-widget py-4 border-top">
             <div class="container">
                 <div class="row col-12">
-                    <?php if ( is_active_sidebar( 'footer1_sidebar' )) : ?>
+                    
                         <div id="footer1" class="col-12 col-md-4 col-lg-4"><?php dynamic_sidebar( 'footer1_sidebar' ); ?></div>
-                    <?php endif; ?>
-                    <?php if ( is_active_sidebar( 'footer2_sidebar' )) : ?>
+                    
                         <div id="footer2" class="col-12 col-md-4 col-lg-4"><?php dynamic_sidebar( 'footer2_sidebar' ); ?></div>
-                    <?php endif; ?>
-                    <?php if ( is_active_sidebar( 'footer3_sidebar' )) : ?>
-                        <div id="footer3" class="col-12 col-md-4 col-lg-4"><?php dynamic_sidebar( 'footer3_sidebar' ); ?></div>
-                    <?php endif; ?>
+                    
+                        <div id="footer3" class="col-12 col-md-4 col-lg-4 float-right"><?php dynamic_sidebar( 'footer3_sidebar' ); ?></div>
+                    
                 </div>
             </div>
         </div>
