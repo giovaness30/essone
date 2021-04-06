0f4198dc82654b7085b14e23b190b6505fb9589d
Giovane Sedano <35101491+giovaness30@users.noreply.github.com>
2021-04-06 13:54:53 -0300

    Adicionado nova função de usuario ao wordpress

--- functions.php
+++ functions.php
@@ -368,3 +368,10 @@ function md_custom_woocommerce_checkout_fields( $fields )
 add_filter( 'woocommerce_checkout_fields', 'md_custom_woocommerce_checkout_fields' );
 
 }
+
+/* Adiciona nova função de usuário */
+function essone_custom_roles(){
+  add_role('contributor_essone_lv','Colaborador Loja Virtual', array('read_shop_order' => true,'edit_shop_orders' => true, 'read' => true, 'edit_posts' => true
+));
+}
+add_action('init','essone_custom_roles');
